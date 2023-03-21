06cdb5e1b21d907956b0705dc5bfdfcc23f47683
astaroth0532 <125462186+astaroth0532@users.noreply.github.com>
2023-03-21 14:54:38 +0200

    11111final version

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -7,7 +7,6 @@ namespace Lab2
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
-
         /// <summary>
         /// Clean up any resources being used.
         /// </summary>
@@ -29,100 +28,113 @@ namespace Lab2
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this._normBtn = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
+            this._AbsBtn = new System.Windows.Forms.Button();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this._resLbl = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // _normBtn
             // 
-            this.button1.Location = new System.Drawing.Point(12, 306);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
+            this._normBtn.Location = new System.Drawing.Point(12, 218);
+            this._normBtn.Name = "_normBtn";
+            this._normBtn.Size = new System.Drawing.Size(75, 23);
+            this._normBtn.TabIndex = 0;
+            this._normBtn.Text = "Норма";
+            this._normBtn.UseVisualStyleBackColor = true;
+            this._normBtn.MouseClick += new System.Windows.Forms.MouseEventHandler(this._normBtn_MouseClick);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(82, 51);
+            this.textBox1.Location = new System.Drawing.Point(195, 51);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(68, 20);
             this.textBox1.TabIndex = 1;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox1_KeyPress);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(82, 86);
+            this.textBox2.Location = new System.Drawing.Point(195, 86);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(68, 20);
             this.textBox2.TabIndex = 1;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox2_KeyPress);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(27, 54);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(165, 13);
             this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.label1.Text = "Введіть дійсну частину а(або x):";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(27, 89);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(167, 13);
             this.label2.TabIndex = 2;
-            this.label2.Text = "label1";
+            this.label2.Text = "Введіть уявну частину bi (або y):\r\n";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(27, 179);
+            this.label3.Location = new System.Drawing.Point(27, 23);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(334, 13);
             this.label3.TabIndex = 2;
-            this.label3.Text = "label1";
+            this.label3.Text = "Програма працює з комплексними числами за замовчуванням.\r\n";
             // 
-            // button2
+            // _AbsBtn
             // 
-            this.button2.Location = new System.Drawing.Point(374, 306);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 0;
-            this.button2.Text = "button1";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button2_MouseClick);
+            this._AbsBtn.Location = new System.Drawing.Point(370, 218);
+            this._AbsBtn.Name = "_AbsBtn";
+            this._AbsBtn.Size = new System.Drawing.Size(75, 23);
+            this._AbsBtn.TabIndex = 0;
+            this._AbsBtn.Text = "Модуль";
+            this._AbsBtn.UseVisualStyleBackColor = true;
+            this._AbsBtn.MouseClick += new System.Windows.Forms.MouseEventHandler(this._AbsBtn_MouseClick);
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(93, 178);
+            this.checkBox1.Location = new System.Drawing.Point(30, 128);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.Size = new System.Drawing.Size(336, 17);
             this.checkBox1.TabIndex = 3;
-            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.Text = "Поставте галочку для переходу у режим роботи з векторами";
             this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.Form1_Load);
+            // 
+            // _resLbl
+            // 
+            this._resLbl.AutoSize = true;
+            this._resLbl.Location = new System.Drawing.Point(27, 181);
+            this._resLbl.Name = "_resLbl";
+            this._resLbl.Size = new System.Drawing.Size(0, 13);
+            this._resLbl.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(461, 341);
+            this.ClientSize = new System.Drawing.Size(457, 253);
+            this.Controls.Add(this._resLbl);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this._AbsBtn);
+            this.Controls.Add(this._normBtn);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -133,14 +145,15 @@ namespace Lab2
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button _normBtn;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button _AbsBtn;
         private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label _resLbl;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,72 +7,80 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using static Lab2.Program;
 namespace Lab2
 {
     public partial class Form1 : Form
     {
+        private readonly Complex _complex = new Complex();
+        private readonly Vector _vector = new Vector();
         public Form1()
         {
             InitializeComponent();
         }
         int a;
         int bi;
+        int x;
+        int y;
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
+        
+        private void TextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if ((e.KeyChar <= 47 || e.KeyChar >= 58) && number != 8 && (e.KeyChar <= 39 ||
+                e.KeyChar >= 46) && number != 47 && number != 61) // цифры, клавиша BackSpace, запятая и математические знаки
+            {
+                e.Handled = true;
+            }
+        }
 
-        private void button1_MouseClick(object sender, MouseEventArgs e)
+        private void TextBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if ((e.KeyChar <= 47 || e.KeyChar >= 58) && number != 8 && (e.KeyChar <= 39 ||
+                e.KeyChar >= 46) && number != 47 && number != 61) // цифры, клавиша BackSpace, запятая и математические знаки
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void _normBtn_MouseClick(object sender, MouseEventArgs e)
         {
             a = Int32.Parse(textBox1.Text);
             bi = Int32.Parse(textBox2.Text);
+            x = Int32.Parse(textBox1.Text); //ініціалізація змінних
+            y = Int32.Parse(textBox2.Text);
+            if (checkBox1.Checked) //викликання методів норми
+            {
+                _resLbl.Text = "Норма вектору дорівнює: " + Convert.ToString(_vector.NORM(x, y)); 
+            }
+            else
+            {
+                _resLbl.Text = "Норма комплексного числа дорівнює: " + Convert.ToString(_complex.NORM(a, bi));
+            }
+            
         }
 
-        private void button2_MouseClick(object sender, MouseEventArgs e)
+        private void _AbsBtn_MouseClick(object sender, MouseEventArgs e)
         {
             a = Int32.Parse(textBox1.Text);
-            bi = Int32.Parse(textBox2.Text);
+            bi = Int32.Parse(textBox2.Text); //ініціалізація змінних
+            x = Int32.Parse(textBox1.Text);
+            y = Int32.Parse(textBox2.Text);
+            if (checkBox1.Checked) //викликання методів модуля
+            {
+                _resLbl.Text = "Модуль вектора дорівнює: " + Convert.ToString(_vector.ABS(x, y));
+            }
+            else
+            {
+                _resLbl.Text = "Модуль комплексного числа дорівнює: " + Convert.ToString(_complex.ABS(a, bi));
+            }
+
         }
     }
-    abstract class Norma
-    {
-        public double Norm;
-        public double Abs;
-        public virtual void ABS()
-        {
-            Console.WriteLine("ABS");
-        }
-        public virtual void NORM()
-        {
-            Console.WriteLine("NORM");
-        }
-    }
-    class Complex : Norma
-    {
-        public override void ABS()
-        {
-            Math.Sqrt();
-        }
-        public override void NORM()
-        {
-            
-            Norm = Math.Pow(a, 2) + Math.Pow(bi, 2);
-            base.NORM();
-        }
-    }
-    class Vector : Norma
-    {
-        public override void ABS()
-        {
-            Math.Sqrt();
-        }
-        public override void NORM()
-        {
-            
-            Norm = Math.Pow(a, 2) + Math.Pow(bi, 2);
-            base.NORM();
-        }
-    }
+    
 }
 
